7d2c32df263ce2952c2ac8f73d589aafae8d372c
Krunal Bhandekar <krunalbhandekar10@gmail.com>
2025-06-17 11:23:41 +0530

    feat(terraform): Added steps for how to implement terraform workspace

---
+++ terraform/6. workspace/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "tf_bucket" {
+  bucket = "my-bucket-${terraform.workspace}" # This uses 'terraform.workspace' to create unique resources per workspace.
+}
+
